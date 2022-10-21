9518dc2a5aae8d6be4b5850b1c96c2a732d4e3d4
Louie Knolle <91914975+louieknolle@users.noreply.github.com>
2022-10-20 21:01:30 -0400

    basic header

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-
+import Header from './Header';
 const App = () => {
   return (
     <React.Fragment>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Header />
     </React.Fragment>
   )
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,10 @@ import PropTypes from 'prop-types'
 const Header = props => {
   return (
     <React.Fragment>
-      
+      <div className='bg-teal-500 text-center text-sky-50 p-20 m-auto '>
+        <h1 className='text-3xl' >If it Ain't Burr Oak Inventory Manager</h1>
+        <h4 className='text-xl pt-2'>Create plantings, sell trees, view Oak cultivars & manage planting details</h4>
+      </div>
     </React.Fragment>
   )
 }
